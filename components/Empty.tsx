e5671646e90cee6d7ec2ff2a705a0118fb75574f
Muse <musemy8484@gmail.com>
2026-02-27 08:13:20 +0300

    feat: change empty image

--- components/Empty.tsx
+++ components/Empty.tsx
@@ -7,7 +7,7 @@ export default function Empty({ lable }: EmptyProps) {
   return (
     <div className="h-full p-20 flex flex-col items-center">
       <div className="relative w-72 h-72">
-        <Image src="/empty.jpg" alt="empty" fill />
+        <Image src="/empty.png" alt="empty" fill />
       </div>
       <p className="text-muted-foreground text-sm text-center">{lable}</p>
     </div>
